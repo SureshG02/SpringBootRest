629e1927bdb8b80c00b322f1af6a5fc32aef2cab
Munnesh <suresh.gupta405@gmail.com>
2017-11-03 07:46:50 +0530

    Intial commit

---
+++ src/main/java/com/iceye/restclient/RestClient.java
@@ -0,0 +1,21 @@
+package com.iceye.restclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.iceye.model.User;
+
+@Component("restClient")
+public class RestClient {
+	
+	@Autowired
+    private RestTemplate restTemplate;
+
+	public ResponseEntity<User[]> fetchUsers() {
+		 
+		return restTemplate.getForEntity("https://jsonplaceholder.typicode.com/posts", User[].class);
+	}
+
+}
